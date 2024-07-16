92f4dab16d498d335aba3d5e505d99a0e2f0a640
Charles Anderson <charlesandreanderson22@gmail.com>
2024-07-16 16:40:34 -0400

    fix react router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 // Entry point
 import App from "./App";
 // Main Layouts
@@ -7,7 +7,7 @@ import { ConsumerLayout } from "./consumer/Layout";
 // Pages
 import { Home } from "./consumer/pages/Home";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <App />,
